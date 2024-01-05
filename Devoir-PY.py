fc6d37e3366296155c2886aba8e785a8a0b405ee
V6Raptor <144737200+V6Raptor@users.noreply.github.com>
2024-01-05 21:40:22 +0100

    Update Devoir-PY.py

--- Devoir-PY.py
+++ Devoir-PY.py
@@ -98,4 +98,172 @@ def etape6(html, balise, attribut):
 
     print(balise_print)
     print(attribut_print)
-... (95 lignes restantes)
+
+
+html_doc = """
+<html><head><title>The Dormouse's story</title></head>
+
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+test_attribut = 'a'
+test_balise = 'p'
+
+
+test_html = etape6(html_doc, test_balise, test_attribut)
+
+
+####################################################
+#####################  Etape 7 #####################
+####################################################
+
+
+def etape7(html, balise, attribut):
+    recup_txt = BeautifulSoup(html, 'lxml')
+    balise_print = recup_txt.find_all(balise)
+    convert = recup_txt.get_text()
+    # attribut_print = balise_print.find_all(attribut)
+
+
+    print(balise_print)
+    print(convert)
+    # print(attribut_print)
+
+
+html_doc = """
+<html><head><title>The Dormouse's story</title></head>
+
+<p class="title"><b>The Dormouse's story</b></p>
+
+<img src="pic_trulli.jpg" alt="Italian Trulli">
+<img src="img_girl.jpg" alt="Girl in a jacket">
+<img src="img_chania.jpg" alt="Flowers in Chania">
+<a href="https://www.w3schools.com">Visit W3Schools</a>
+<a href="https://esiee-it.blackboard.com/ultra/courses/_101859_1/outline/edit/document/_1173113_1?courseId=_101859_1&view=content">ESIEE</a>
+<a href="https://google.com">Navigateur</a>
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+
+test_balise = 'img'
+test_attribut = 'alt'
+
+test_html = etape7(html_doc, test_balise, test_attribut)
+
+
+####################################################
+#####################  Etape 8 #####################
+####################################################
+
+
+def etape8(url):
+    nom_domaine = urlparse(url).netloc
+    print(nom_domaine)
+
+
+url_test = 'https://google.com'
+
+test_html = etape8(url_test)
+
+
+####################################################
+#####################  Etape 9 #####################
+####################################################
+
+
+def etape9(nom_domaine, urls):
+    urls_du_domaine = []
+    urls_hors_domaine = []
+
+    for url in urls:
+        if nom_domaine in url:
+            urls_du_domaine.append(url)
+        else:
+            urls_hors_domaine.append(url)
+
+    return urls_du_domaine, urls_hors_domaine
+
+
+
+nom_domaine = "example.com"
+liste_urls = [
+    "http://www.example.com/page1",
+    "http://subdomain.example.com/page2",
+    "https://anotherdomain.com",
+    "http://www.example.org",
+    "https://example.com/page3"
+]
+
+urls_du_domaine, urls_hors_domaine = trier_urls_par_domaine(nom_domaine, liste_urls)
+
+print("URLs du domaine : ", urls_du_domaine)
+print("URLs hors du domaine : ", urls_hors_domaine)
+
+
+####################################################
+#####################  Etape 10 ####################
+####################################################
+
+def etape10(url):
+    page = requests.get(url)
+    html = page.content
+    texte_recup = BeautifulSoup(html, 'lxml')
+    print(texte_recup)
+
+
+
+lien = 'https://mc-projet-bts.alwaysdata.net/vitrine'
+
+test_html2 = etape10(lien)
+
+
+####################################################
+#####################  Etape 11 ####################
+####################################################
+
+
+def etape11(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        keywords = soup.find('meta', attrs={'name': 'keywords'})
+        if keywords:
+            keywords_content = keywords.get('content')
+            mots_cles = keywords_content.split(',')[:3]
+            occurrences = {mot: keywords_content.count(mot) for mot in mots_cles}
+        else:
+            mots_cles = []
+            occurrences = {}
+
+        liens_entrants = len(soup.find_all('a', href=True))
+
+        liens_sortants = len([link for link in soup.find_all('a') if link.get('href').startswith('http')])
+
+        balises_alt = soup.find_all('img', alt=True)
+        presence_balises_alt = len(balises_alt) > 0
+
+        print("Mots-clés et leurs 3 premières occurrences :", occurrences)
+        print("Nombre de liens entrants :", liens_entrants)
+        print("Nombre de liens sortants :", liens_sortants)
+        print("Présence de balises alt pour les images :", presence_balises_alt)
+    else:
+        print("La requête a échoué avec le code :", response.status_code)
+
+url_page = input("Entrez l'URL de la page à analyser : ")
+
+audit_page(url_page)
+
+
